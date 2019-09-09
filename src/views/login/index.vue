a54696200eaa0e5cab42961a7c86a91fbe1835a9
como <jivedon@163.com>
2019-09-09 21:41:13 +0800

    增加路由

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="hello">
-    <!-- <h1>{{ msg }}</h1> -->
-        <h1>como</h1>
-
+    <h1>como</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
